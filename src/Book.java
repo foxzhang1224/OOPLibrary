171c89afe580a406a53b1b6b478d61c26daa4e09
danny031103 <db4471@nyu.edu>
2024-12-06 15:51:48 -0500

    Added setters+getters and fields to Book class

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,64 @@
 package src;
 
 public class Book {
-    
+    public int id;
+    public String title;
+    public String author;
+    public int yearofpublication;
+    public String genre;
+    public boolean isBorrowed;
+
+
+//getters and setters
+
+//id getter
+public int getId() {
+    return id;
+}
+
+//id setter
+public void setId(int id) {
+    this.id=id;
+}
+
+//title getter
+public String getTitle() {
+    return title;
+}
+
+//title setter
+public void setTitle(String title) {
+    this.title=title;
+}
+
+//author getter
+public String getAuthor() {
+    return author;
+}
+
+//author setter
+public void setAuthor(String author) {
+    this.author=author;
+}
+
+//year getter
+public int getYear() {
+    return yearofpublication;
+}
+
+//year setter
+public void setYear(int yearofpublication) {
+    this.yearofpublication=yearofpublication;
+}
+
+//genre getter
+public String getGenre() {
+    return genre;
+}
+
+//genre setter
+public void setGenre(String genre) {
+    this.genre=genre;
+}
+
 }
